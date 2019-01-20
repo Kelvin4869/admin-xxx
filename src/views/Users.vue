9b035e29e54037603859b998629cf31a8c47d7ba
Kelvin <kuangsixiang@163.com>
2019-01-20 14:35:44 +0800

    用户列表静态页面

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -7,14 +7,71 @@
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
     <div>
-      <el-input placeholder="请输入内容" v-model="input5" class="input-with-select">
-        <el-select v-model="select" slot="prepend" placeholder="请选择">
-          <el-option label="餐厅名" value="1"></el-option>
-          <el-option label="订单号" value="2"></el-option>
-          <el-option label="用户电话" value="3"></el-option>
-        </el-select>
+      <el-input placeholder="请输入内容" v-model="searchVal" class="search-input">
         <el-button slot="append" icon="el-icon-search"></el-button>
       </el-input>
+      <el-button type="primary" plain>添加用户</el-button>
     </div>
+    <el-table :data="tableData" border style="width: 100%" class="mt-15 mb-15">
+      <el-table-column prop="date" label="日期" width="180">
+      </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180">
+      </el-table-column>
+      <el-table-column prop="address" label="地址">
+      </el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination
+      class="page"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage4"
+      :page-sizes="[100, 200, 300, 400]"
+      :page-size="100"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400">
+    </el-pagination>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      searchVal: '',
+      currentPage4: 4,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  },
+  methods: {
+    handleSizeChange (val) {
+      console.log(`每页 ${val} 条`)
+    },
+    handleCurrentChange (val) {
+      console.log(`当前页: ${val}`)
+    }
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+.search-input {
+    width: 300px;
+    margin-right: 20px;
+}
+</style>
